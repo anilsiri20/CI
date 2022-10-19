fd50181885dc7143101378038e3488aa7058a160
anil <106726513+anilsiri20@users.noreply.github.com>
2022-10-19 23:40:56 +0530

    poll scm change

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello SAAHO ;;anil World!" );
+        System.out.println( "Hello pll scm change!" );
     }
 }
