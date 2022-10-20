5803d656554eb2e5f85d5dc4291e8abb4fb4c5e9
anil <106726513+anilsiri20@users.noreply.github.com>
2022-10-20 17:31:44 +0530

    github commit

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello github pll scm change!" );
+        System.out.println( "Hello github github pll scm change!" );
     }
 }
